a21e9c85d8442693e698b1da603cdd3d6126c9a9
Jomr02 <78909730+Jomr02@users.noreply.github.com>
2024-08-19 20:33:44 +0200

    Add Naive Bayes

---
+++ Naive_Bayes/Naive_Bayes.py
@@ -0,0 +1,38 @@
+import pandas as pd  
+import numpy as np
+import os
+from sklearn.naive_bayes import GaussianNB 
+from sklearn import metrics 
+
+
+full_path = os.path.realpath(__file__)
+dir_path = os.path.dirname(full_path)
+
+X_train = pd.read_csv(f'{dir_path}/UNSW-NB15-train-set.csv') 
+y_train = pd.read_csv(f'{dir_path}/UNSW-NB15-train-set-label.csv') 
+X_test = pd.read_csv(f'{dir_path}/UNSW-NB15-test-set.csv') 
+y_test = pd.read_csv(f'{dir_path}/UNSW-NB15-test-set-label.csv')
+    
+
+#To reshape y_train into 1d array
+y_train = y_train['label'] 
+    
+#Create a Gaussian Classifier
+model = GaussianNB()
+
+#Train the model using the training sets
+model.fit(X_train,y_train) 
+
+y_pred = model.predict(X_test)  
+
+
+# Medir la precisión de naive Bayes
+print("Precisión:",metrics.accuracy_score(y_test, y_pred))
+
+report=metrics.classification_report(y_test, y_pred)
+
+DTclf_name=['Naive Bayes','RegLog']
+
+print('Resultados: %s:'%DTclf_name)
+
+print(report)
